53d030f5655a8d25492fe78c60f32394d26318dc
Dima Usatyuk <Shonen1988@gmail.com>
2020-03-20 11:40:14 +0200

    just some practice

--- app/src/main/java/com/shonen/ukr/animationsinandroid/MainActivity.java
+++ app/src/main/java/com/shonen/ukr/animationsinandroid/MainActivity.java
@@ -9,6 +9,7 @@ import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private TextView txtHelloWorld;
+    private TextView txtHiWorld;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -16,10 +17,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
            txtHelloWorld = findViewById(R.id.txtHelloWorld);
+           txtHiWorld = findViewById(R.id.txtHiWorld);
            txtHelloWorld.setOnClickListener(new View.OnClickListener() {
                @Override
                public void onClick(View v) {
-                   Log.i("Mylog", "Button was clicked");
+//                   Log.i("Mylog", "Button was clicked");
+//                   txtHelloWorld.animate().rotation(90f).setDuration(1000);
+                   txtHelloWorld.animate().alpha(0.f).setDuration(5000);
+                   txtHiWorld.animate().alpha(1f).setDuration(3000);
                }
            });
     }
